2345e198395cb069d930abdfc9edc0c2c8b8b112
bpavan <pavan.kumar@idexcel.com>
2018-05-09 17:02:52 +0530

    commit

--- s3bucket/recipes/default.rb
+++ s3bucket/recipes/default.rb
@@ -6,5 +6,6 @@
 aws_s3_file 'SampleApp_Linux.zip' do
   bucket 'ansible-poc-idexcel'
   remote_path '/home/ubuntu'
+  s3_url "https://s3.amazonaws.com/ansible-poc-idexcel/SampleApp_Linux.zip"
   region 'us-east-1'
 end
