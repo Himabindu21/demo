2fd9bb9636b7d91035bbbbd94927826c5b0db95f
bpavan <pavan.kumar@idexcel.com>
2018-05-10 12:32:56 +0530

    commit

--- s3bucket/recipes/default.rb
+++ s3bucket/recipes/default.rb
@@ -3,7 +3,7 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
-aws_s3_ file 'welcome.txt' do
+s3_ file 'welcome.txt' do
   bucket 'ansible-poc-idexcel'
   s3_url "https://s3.amazonaws.com/ansible-poc-idexcel/welcome.txt"
   remote_path '/home/ubuntu'
